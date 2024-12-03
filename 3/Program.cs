4b59a131d78170f6bc1c5d3a01396d1ed5f1bbf8
Jean-Philippe Green <jeanphilippe.green@protonmail.com>
2024-12-03 22:52:13 +0100

    Day 3

---
+++ 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.VisualBasic.FileIO;
+
+StreamReader sr = new("./input.txt");
+
+var wholeInput = sr.ReadToEnd();
+
+/***************************** PART 1 *********************************/
+static int Part1(string input)
+{
+    var regex = new Regex(@"mul\((\d+),(\d+)\)");
+
+    var sum = 0;
+
+    foreach (Match match in regex.Matches(input))
+    {
+        var lhs = match.Groups.Values.ElementAt(1).Value;
+        var rhs = match.Groups.Values.ElementAt(2).Value;
+        sum += int.Parse(lhs) * int.Parse(rhs);
+    }
+    return sum;
+}
+Console.WriteLine(Part1(wholeInput));
+
+/***************************** PART 2 *********************************/
+
+var inputSplitOnDont = wholeInput.Split("don't");
+
+// First part is special because it starts enabled
+var sum = Part1(inputSplitOnDont.First());
+
+// After each "don't", we have to find a "do"
+foreach(var inputPart in inputSplitOnDont.Skip(1)) {
+    var inputSplitOnDo = inputPart.Split("do");
+    // After the "do", we're good to go.
+    var inputPartToDo = string.Join("", inputSplitOnDo.Skip(1));
+
+    sum += Part1(inputPartToDo);
+}
+Console.WriteLine(sum);
