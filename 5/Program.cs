fd0a5fe75738799d32b79b9c745a06b1f7e4daeb
Jean-Philippe Green <jean.philippe.green@brilliantfuture.se>
2024-12-23 23:29:21 +0100

    Day 5 - part 1

---
+++ 5/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Formats.Tar;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+StreamReader sr = new("../../../input.txt");
+
+var input = sr.ReadToEnd();
+var splitInput = input.Split("\n\n");
+
+var rules = splitInput[0].Split("\n");
+var updates = splitInput[1].Split("\n");
+
+var part1 = 0;
+
+foreach (var update in updates)
+{
+    var pages = update.Split(",");
+    if (pages.Length == 1)
+    {
+        continue;
+    }
+    string[] correctedPages = [.. pages];
+    var errors = false;
+
+    foreach (var rule in rules)
+    {
+        var ruleSplit = rule.Split('|');
+        var ruleBefore = ruleSplit[0];
+        var ruleAfter = ruleSplit[1];
+
+        var indexBefore = Array.IndexOf(pages, ruleBefore);
+        var indexAfter = Array.IndexOf(pages, ruleAfter);
+
+        if (indexBefore != -1 && indexAfter != -1 && indexBefore > indexAfter)
+        {
+            errors = true;
+            break;
+        }
+    }
+
+    if (!errors)
+    {
+        var middle = pages[pages.Length / 2];
+        part1 += int.Parse(middle);
+    }
+}
+
+Console.WriteLine(part1);
